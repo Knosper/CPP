59cbe0a1744a762b83c265b292dbeeb5cc2867c1
jjesberg <jjesberg@student.42heilbronn.de>
2022-11-22 15:59:36 +0100

    bureaucrats

--- CPP_5/ex00/main.cpp
+++ CPP_5/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjesberg <j.jesberger@heilbronn.de>        +#+  +:+       +#+        */
+/*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:15:43 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/11/21 13:17:43 by jjesberg         ###   ########.fr       */
+/*   Updated: 2022/11/22 15:55:57 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,35 @@
 int     main()
 {
     Bureaucrat  first("Hannes");
+   	Bureaucrat  sec;
+    Bureaucrat  fourth = first;
+    Bureaucrat  third(fourth);
+    Bureaucrat  five(12);
+    Bureaucrat  six("Peter", 2);
+
+	try
+	{
+		first.setGrade(149);
+	}
+	catch(Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	fourth.incrementGrade(100);
+	first.incrementGrade(12);
+	six.incrementGrade(1000);
+
+	std::cout << first << std::endl;
+	std::cout << sec << std::endl;
+	std::cout << third << std::endl;
+	std::cout << fourth << std::endl;
+	std::cout << five << std::endl;
+	std::cout << six << std::endl;
 
     return (0);
 }
