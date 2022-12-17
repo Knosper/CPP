3b8896600a4adadc611221af97f2b2126fc26b25
Julian Jesberger <jjesberg@2-F-7.42heilbronn.de>
2022-12-17 20:35:30 +0100

    ex08

--- CPP_8/ex02/MutantStack.hpp
+++ CPP_8/ex02/MutantStack.hpp
@@ -3,25 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: jjesberg <j.jesberger@heilbronn.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 02:35:36 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/12/07 02:51:46 by jjesberg         ###   ########.fr       */
+/*   Updated: 2022/12/17 20:34:27 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
 
+# include <stack>
 # include <string>
 # include <iostream>
-# include "MutantStack.tpp"
+# include <algorithm>
+# include <iterator>
+# include <ctime>
+# include <cstdlib>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
 
-class MutantStack
+template <typename T>
+class	MutantStack : public std::stack<T> 
 {
-private:
+	public :
+		
+	MutantStack(void) : std::stack<T>() { }
+	MutantStack(MutantStack const &ref) : std::stack<T>(ref) { }
 
-public:
-	MutantStack();
-	~MutantStack();
+	virtual ~MutantStack(void) { }
+
+	MutantStack &operator=(MutantStack const &ref) 
+	{
+		this->c = ref.c;
+		return (*this); 
+	}
+
+	typedef typename	std::stack<T>::container_type::iterator							iterator;
+	typedef typename	std::stack<T>::container_type::const_iterator					const_iterator;
+	typedef typename	std::stack<T>::container_type::reverse_iterator					reverse_iterator;
+	typedef typename	std::stack<T>::container_type::const_reverse_iterator			const_reverse_iterator;
+
+	iterator				begin(void)					{ return (this->c.begin()); }
+	iterator				end(void)					{ return (this->c.end()); }
+
+	const_iterator			begin(void)	const			{ return (this->c.begin()); }
+	const_iterator			end(void)	const			{ return (this->c.end()); }
+	reverse_iterator		rbegin(void)				{ return (this->c.rbegin()); }
+	reverse_iterator		rend(void)					{ return (this->c.rend()); }
+
+	const_reverse_iterator	rbegin(void) 	const	{ return (this->c.rbegin()); }
+	const_reverse_iterator	rend(void)		const	{ return (this->c.rend()); }
 };
-
--- CPP_8/ex02/main.cpp
+++ CPP_8/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: jjesberg <j.jesberger@heilbronn.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:46:32 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/12/07 02:40:02 by jjesberg         ###   ########.fr       */
+/*   Updated: 2022/12/17 20:31:47 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 int main() 
 {
-	
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "Size = " << mstack.size() << std::endl;
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Size = " << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 	return 0;
 }
